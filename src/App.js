3138f2d8c13d7c7ca998d73d7f40f9facae3d906
ukyung817 <youu8017@naver.com>
2025-06-25 23:41:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './components/section/Header'
+import Homepage from './components/main/Homepage'
+import Footer from './components/section/Footer'
+import Search from './components/main/Search';
+
+
+
+const App = () => {
+  return (
+    <>
+    
+    <Header /> 
+    <Homepage />
+    <Footer />
+    <Search />
+    
+
+    </>
+  )
+}
+
+export default App;
